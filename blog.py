1693d4eae8c918be814dcb0446f5440f867a84ec
Nakul Pathak <nakulpathak3@hotmail.com>
2014-11-29 14:28:52 -0500

    Added some functions to blog and deleted unused stylesheet. Added front.html to blog so it displays all the posts from the database.

--- blog.py
+++ blog.py
@@ -3,16 +3,28 @@ import re
 import webapp2
 import jinja2
 import time
+import hashlib
+import hmac
 
 from string import letters
+from secret import *
 
 from google.appengine.ext import db
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
                                                                 autoescape = True)
+###################HASHING FUNCTIONS##################################
+def hash_str(s):
+    return hmac.new(secret, s).hexdigest()
 
+def make_secure_val(s):
+    return "%s|%s" % (s, hash_str(s))
 
+def check_secure_val(h):
+    val = h.split('|')[0]
+    if h == make_secure_val(val):
+        return val
 
 class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
@@ -25,7 +37,18 @@ class Handler(webapp2.RequestHandler):
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
 
+    def set_cookie(self, name, val):
+        cookie_val = make_secure_val(val)
+        self.response.headers.add_header('Set-Cookie', '%s=%s' % (name, cookie_val))
+
+    def read_cookie(self, name, val):
+        cookie_val = self.request.cookies.get(name)
+        if cookie_val and check_secure_val(cookie_val):
+            return cookie_val
+
 class Blog(db.Model):
+    #user = db.StringProperty(required = True)
+    #password = db.StringProperty(required = True)
     subject = db.StringProperty(required = True)
     blog = db.TextProperty(required = True)
     created = db.DateTimeProperty(auto_now_add = True)
@@ -65,13 +88,30 @@ class FormPage(Handler):
             error = "we need both a subject and a body"
             self.render(subject, blog, error)
 
+
+#Signup to work on. For hashing and validity checks
+#Welcome page of user using cookie
+#Add projects to website and uploading!
+#Changes to CSS of template to make it not seem like a complete copy paste.
+
 class Permalink(MainPage):
     def get(self, blog_id):
         s = Blog.get_by_id(int(blog_id))
         self.render("front.html", blogs=[s])
 
+
 class SignUp(Handler):
     def get(self):
+        visits = 0
+        visit_cookie_str  = self.request.cookies.get('visits')
+        if visit_cookie_str:
+            cookie_val = check_secure_val(visit_cookie_str)
+            if cookie_val:
+                visits = int(cookie_val)
+        visits += 1
+        new_cookie_val = make_secure_val(str(visits))
+        self.response.headers.add_header('Set-Cookie', 'visits=%s' % new_cookie_val)
+        self.write("You've been here %s times!\n" % visits)
         self.render("signup.html")
 
     def post(self):
@@ -83,10 +123,21 @@ class SignUp(Handler):
         if username and password and ver_password and (password == ver_password):
             self.redirect("/welcome")
 
+class LogIn(Handler):
+    def get(self):
+        self.render("login.html")
+
+    def post(self):
+        username = self.request.get("username")
+        password = self.request.get("password")
+        if check_secure_val(make_secure_val(password)):
+            self.redirect('/welcome')
+
 app = webapp2.WSGIApplication([ ('/', IndexPage),
                                 ('/blog', MainPage),
                                 ('/newpost', FormPage),
                                 ('/blog/(\d+)', Permalink),
-                                ('/signup', SignUp)
+                                ('/signup', SignUp),
+                                ('/login', LogIn)
                                ],
                                 debug = True)
